e374a18d41995b45c93f080a56a036e77ef12a52
motohiro84 <motogen.8040-1995@docomo.ne.jp>
2021-01-22 16:50:16 +0900

    ランダムな行動

--- Assets/Scripts/MoveEnemy.cs
+++ Assets/Scripts/MoveEnemy.cs
@@ -11,29 +11,57 @@ public class MoveEnemy : MonoBehaviour
   private float walkSpeed = 1.0f;
   private Vector3 velocity;
   private Vector3 direction;
+  private bool arrived;
+  private SetPosition setPosition;
+  [SerializeField]
+  private float waitTime = 3f;
+  private float elapsedTime;
 
   void Start()
   {
+    setPosition = GetComponent<SetPosition>();
     enemyController = GetComponent<CharacterController>();
     animator = GetComponent<Animator>();
-    destination = new Vector3(0f, 0f, 0f);
+    setPosition.CreateRandomPosition();
     velocity = Vector3.zero;
+    arrived = false;
+    elapsedTime = 0f;
   }
 
 
   // Update is called once per frame
   void Update()
   {
-    if (enemyController.isGrounded)
+    if (!arrived)
     {
-      velocity = Vector3.zero;
-      animator.SetFloat("Speed", 2.0f);
-      direction = (destination - transform.position).normalized;
-      transform.LookAt(new Vector3(destination.x, transform.position.y, destination.z));
-      velocity = direction * walkSpeed;
-      Debug.Log(destination);
+      if (enemyController.isGrounded)
+      {
+        velocity = Vector3.zero;
+        animator.SetFloat("Speed", 2.0f);
+        direction = (destination - transform.position).normalized;
+        transform.LookAt(new Vector3(destination.x, transform.position.y, destination.z));
+        velocity = direction * walkSpeed;
+        // Debug.Log(destination);
+      }
+      velocity.y += Physics.gravity.y * Time.deltaTime;
+      enemyController.Move(velocity * Time.deltaTime);
+
+      if (Vector3.Distance(transform.position, destination) < 0.5f)
+      {
+        arrived = true;
+        animator.SetFloat("Speed", 0.0f);
+      }
     }
-    velocity.y += Physics.gravity.y * Time.deltaTime;
-    enemyController.Move(velocity * Time.deltaTime);
+    else
+    {
+      elapsedTime += Time.deltaTime;
+      if (elapsedTime > waitTime)
+      {
+        setPosition.CreateRandomPosition();
+        destination = setPosition.GetDestination();
+        arrived = false;
+        elapsedTime = 0f;
+      }
+    }
   }
 }
---
+++ Assets/Scripts/SetPosition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPosition : MonoBehaviour
+{
+  private Vector3 startPosition;
+  private Vector3 destination;
+
+  void Start()
+  {
+    startPosition = transform.position;
+    SetDestination(transform.position);
+  }
+
+  public void CreateRandomPosition()
+  {
+    var randDestination = Random.insideUnitCircle * 8;
+    SetDestination(startPosition + new Vector3(randDestination.x, 0, randDestination.y));
+  }
+
+  public void SetDestination(Vector3 position)
+  {
+    destination = position;
+  }
+
+  public Vector3 GetDestination()
+  {
+    return destination;
+  }
+
+}
